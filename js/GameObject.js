7ea1cb6fbe0009aca1738c7f67ee354024a229e4
Andrew Berls <andrew.berls@gmail.com>
2012-02-05 13:56:32 -0800

    Generic GameObject class

---
+++ js/GameObject.js
@@ -0,0 +1,34 @@
+/*
+	Class File: GameObject.js	
+	Inherits from: 
+	Attributes:
+		x
+		y		
+		dx
+		dy
+		height
+		width
+	
+	Method Signatures:		
+	
+*/
+
+function GameObject() {	
+
+	// Position
+	this.x = 0;
+	this.y = 0;
+	
+	// Direction
+	this.dx = 0;
+	this.dy = 0;
+	
+	// Box model
+	this.width = 0;
+	this.height = 0;
+	
+	// Sprite
+	this.sprite = new Image();
+	this.sprite.src = "";
+	
+}
