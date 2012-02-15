f4bfa4c92f4227d757e1e7383f6110e5b8957ecf
Andrew Berls <andrew.berls@gmail.com>
2012-02-15 13:45:37 -0800

    Adding setInterval tips

--- js/Utils.js
+++ js/Utils.js
@@ -7,3 +7,22 @@ function log(msg) { console.log(msg); }
 function randomFromTo(from, to){
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
+
+
+
+/*
+// Workarounds for setInterval 'this' context errors
+
+// 1.) Define variable to hold 'this' context
+var self = this;
+setInterval(function() {log(self)}, 1000)
+
+
+// 2.) Funky closure shit
+this.interval = setInterval(
+  (function(self) {
+    return function() {
+      self.retrieve_rate();
+    }
+  })(this), 1000);
+*/
