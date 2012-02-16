a28b31775a01eff37ae21caa019c8ee30954ed7c
Billy Galarpe <billygalarpe@umail.ucsb.edu>
2012-02-15 19:36:49 -0800

    Changes sprite_test bomb dropping
    DOES NOT WORK

--- sprite_test/js/Bomb.js
+++ sprite_test/js/Bomb.js
@@ -12,7 +12,7 @@ function Bomb(playerX, playerY) {
 	this.width = 10;
 	this.height = 5;
 	
-	this.isDropping = false;
+    this.isDropping = false;
 	
 	this.sprite = new Image();
 	this.sprite.src = "images/bomb.gif";
@@ -22,34 +22,51 @@ Bomb.prototype.move = function() {
 	this.y += this.dy;
 };
 
-Bomb.prototype.draw = function() {
+Bomb.prototype.drop = function() {
+    
+    this.isDropping = true;
+    bombSound.play();
+    
+    while(this.y > canvas.height - 30) {
+        this.isDropping = true;
+        this.dy = 6;
+        buffer_ctx.drawImage(this.sprite, this.x, this.y);
+    }
 	
-    /**
+	// Old way of timing bomb reset
+    /*
+	setTimeout(function() {
+		console.log("Resetting bomb and this.isDropping.");
+		this.isDropping = false;				
+		//bomb.reset();
+	}, 2750);
+    
+    */
+    
+    if (this.y == canvas.height - 30) {  
+		explodeSound.play();
+		if (debug_mode) { console.log("Calling reset from ground collision detection"); }
+    }
+    else {
+		return;
+	}
+}
+
+Bomb.prototype.draw = function() {
+
+    // If the bomb is in the air, fall to the ground, else leave in reset
+
     
 	// Has the bomb hit any bricks?
 	// TO DO
 	
-	// Has the bomb hit the ground?
-	if (this.isDropping && this.y >= canvas.height - 30) {  
-		explodeSound.play();
-		this.isDropping = false;
-		//this.reset();
-		if (debug_mode) { console.log("Calling reset from ground collision detection"); }
-		return;
-	}
-	
-	// If the bomb is in the air, fall to the ground, else leave in reset
-	if (this.isDropping) {
-		this.dy = 6;
-		buffer_ctx.drawImage(this.sprite, this.x, this.y);
-	} else {
-		this.dy = 0;
-        //this.reset();
-	}	
-	
-    */
     
-    //Has the bomb hit the ground?
+	// Has the bomb hit the ground?
+    
+    
+    
+	
+    
     
 }
 
@@ -61,25 +78,3 @@ Bomb.prototype.reset = function() {
 		this.y = playerY + player.height;	
 }
 */
-
-Bomb.prototype.drop = function() {
-    
-    
-    
-	if (this.isDropping) {
-		// Do nothing if player presses space while bomb already dropping
-		if (debug_mode) { console.log("Bomb is already dropping."); }		
-		return;
-	}
-	this.isDropping = true;	
-	bombSound.play();
-	
-	// Old way of timing bomb reset
-    /*
-	setTimeout(function() {
-		console.log("Resetting bomb and this.isDropping.");
-		this.isDropping = false;				
-		//bomb.reset();
-	}, 2750);
-    */
-}
