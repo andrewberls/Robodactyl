842e774547a4cf2637ae934f3b7ed0d08cfd7433
Andrew Berls <andrew.berls@gmail.com>
2012-02-22 23:19:18 -0800

    Renaming lasers and adding death/respawn

--- src/js/Audio.js
+++ src/js/Audio.js
@@ -7,12 +7,14 @@
 var themeSong = new Audio("audio/theme_soft50.mp3");
 
 // Weapons
-var laser1 = new Audio("audio/laser1.mp3");
-var laser2 = new Audio("audio/laser2.mp3");
+var player_fire = new Audio("audio/player_fire.mp3");
+var enemy_fire = new Audio("audio/enemy_fire.mp3");
 //var rage_fire = new Audio("");
 
 // Hurt/Kill
 var player_hurt = new Audio("audio/player_hurt.mp3");
+var player_death = new Audio("audio/player_death.mp3");
+var player_respawn = new Audio("audio/respawn.mp3");
 var enemy_death1 = new Audio("audio/enemy_death1.mp3");
 var enemy_death2 = new Audio("audio/enemy_death2.mp3");
 var enemy_death3 = new Audio("audio/enemy_death3.mp3");
@@ -23,5 +25,3 @@ var rage_activate = new Audio("audio/rage_activate.mp3");
 var shield_activate = new Audio("audio/shield_activate.mp3");
 var shield_hit = new Audio("audio/shield_hit2.mp3");
 var shield_deactivate = new Audio("audio/shield_deactivate.mp3");
-
-// Misc
