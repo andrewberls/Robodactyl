1388ac1b00c7977ace229b3e9947f69bc959cc24
Andrew Berls <andrew.berls@gmail.com>
2012-02-22 20:23:17 -0800

    Randomly generated blocks

--- src/js/Block.js
+++ src/js/Block.js
@@ -1,23 +1,30 @@
 /*
-  Class File: Block.js
-  Inherits from: GameObject
-  Attributes:
+    Class File: Block.js
+    Inherits from: GameObject
+    Attributes:
 		
-	
-  Method Signatures:
-    move()
-    draw()
-	
+    Method Signatures:
+        move()
+        draw()
 */
 
 function Block(x,y) {
-  this.x = x;
-  this.y = y;
+    this.x = x;
+    this.y = y;
   
-  this.width = 2*TILE_SIZE;
-  this.height = 2*TILE_SIZE;  
- 
-  blocks.push(this); // Add self to manager array
+    this.width  = 2 * TILE_SIZE;
+    this.height = 2 * TILE_SIZE;
+
+    switch (randomFromTo(2,3)) {
+        case 2:
+            this.sprite.src = "images/block/block2.png";
+            break;
+        case 3:
+            this.sprite.src = "images/block/block3.png";
+            break;        
+    };
+
+    blocks.push(this); // Add self to manager array
 }
 
 Block.prototype = new GameObject();
@@ -25,10 +32,10 @@ Block.prototype.constructor = Block;
 
 Block.prototype.move = function() {}
 
-Block.prototype.draw = function() {  
-  ctx.save();
-  ctx.fillStyle = "orange";
-  ctx.fillRect(this.x, this.y, this.width, this.height);
-  //ctx.drawImage(this.sprite, this.x, this.y);
-  ctx.restore();
+Block.prototype.draw = function() {
+    ctx.save();
+    ctx.fillStyle = "orange";
+    //ctx.fillRect(this.x, this.y, this.width, this.height); // Box model
+    ctx.drawImage(this.sprite, this.x, this.y);
+    ctx.restore();
 }
