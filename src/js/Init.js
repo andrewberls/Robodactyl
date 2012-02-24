15aac9f671114b11e86f3a85ee7543176bad5c9a
Andrew Berls <andrew.berls@gmail.com>
2012-02-24 01:37:24 -0800

    Consistent variable names

--- src/js/Init.js
+++ src/js/Init.js
@@ -29,43 +29,32 @@ var collectables = [], // Powerups, etc
     enemyProjectiles  = [];
 
     
-/* LEVEL OBJECTS
-/----------------------------------*/
-
-/*----------------------------------*/
-
-  // THIS NEEDS TO BE SPLIT UP BY LEVEL!
-
-/*----------------------------------*/
-
 // Environment
 var background = new Background();
 
 // Characters
 var player = new Player();
+  
+    
+/* LEVEL OBJECTS
+/----------------------------------*/
+
+// TEMPORARY INITIALIZATION FOR ITERATION 1
+// THIS NEEDS TO BE SPLIT UP BY LEVEL!
 
 
 /* SCREEN 1
 ----0-------------340--------------780---*/
-
-
-
+// Enemies
 var enemy1 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
 var enemy2 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
 var enemy3 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
 
-// Enemies
-//var enemy1 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
-//var enemy2 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
-//var enemy3 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
-
-//var enemy4 = new Enemy(randomFromTo(0,C_WIDTH-2*TILE_SIZE));
-
 // Blocks
-//var block = new Block(400,100);
+var block = new Block(400,150);
 
 //PowerUp
-//var powerup = new PowerUp(200,200, 1);
+var powerup = new PowerUp(200,200, 1);
 
 
 /* SCREEN 2
@@ -83,7 +72,7 @@ var b_2_3 = new Block(1170,0);
 var b_2_4 = new Block(1170,60);
 var b_2_5 = new Block(1365,0);
 var b_2_6 = new Block(1365,60);
-var b_2_7 = new Block(1365, 275);
+var b_2_7 = new Block(1170, 275);
 
 // Powerups
 var p_2_1 = new PowerUp(1175, 215, 4); // Shield
@@ -108,23 +97,25 @@ var p_3_1 = new PowerUp(1945, 100, 4); // Rage
 /* SCREEN 4
 ---2340-------------2730---------------3120---*/
 // Enemies
-var enemy41 = new Enemy(randomFromTo(2340,2440));
-var enemy42 = new Enemy(randomFromTo(2440,2740));
-var enemy43 = new Enemy(randomFromTo(2740,2900));
-var enemy44 = new Enemy(randomFromTo(2900,3120));
+var e_4_1 = new Enemy(randomFromTo(2340,2440));
+var e_4_2 = new Enemy(randomFromTo(2440,2740));
+var e_4_3 = new Enemy(randomFromTo(2740,2900));
+var e_4_4 = new Enemy(randomFromTo(2900,3120));
+
 // Blocks
-var block41 = new Block(((2730+2340)/2),0);
-var block42 = new Block(((2730+2340)/2),60);
-var block43 = new Block(((2730+2340)/2),120);
-var block44 = new Block(((2730+2340)/2),180);
-var block45 = new Block(((3120+2730)/2),0);
-var block46 = new Block(((3120+2730)/2),60);
-var block47 = new Block(((3120+2730)/2),120);
-var block48 = new Block(((3120+2730)/2),180);
+var b_4_1 = new Block(((2730+2340)/2),0);
+var b_4_2 = new Block(((2730+2340)/2),60);
+var b_4_3 = new Block(((2730+2340)/2),120);
+var b_4_4 = new Block(((2730+2340)/2),180);
+var b_4_5 = new Block(((3120+2730)/2),0);
+var b_4_6 = new Block(((3120+2730)/2),60);
+var b_4_7 = new Block(((3120+2730)/2),120);
+var b_4_8 = new Block(((3120+2730)/2),180);
 
 // Powerups
-var powerup41 = new PowerUp(2730,0, 2);
-
+var p_4_1 = new PowerUp(2730,10, 2); // Health
+  
+  
 /* SCREEN 5
 --3120----------------3510--------------3900--*/
 // Enemies
@@ -132,18 +123,14 @@ var powerup41 = new PowerUp(2730,0, 2);
 // Blocks
 
 // Powerups
-
-
-
-
-
+  
 
 
 /* MAIN INITIALIZATION
 /----------------------------------*/
 function init() {
     
-    var game = new Game();
+    var game = new Game();    
     gameLoop = setInterval(game.draw, 20);
     
     // Display the start menu
@@ -154,7 +141,7 @@ function init() {
     function(option) {   // Function triggered by enter key
       if (option == 0) {
         menuActive = false;
-        //themeSong.play();
+        themeSong.play();
       }
     });
   
