456beaabb63890c6e185f3886e9576e7609ca4cd
Bryce Filler <bfiller23@gmail.com>
2012-03-11 14:36:01 -0700

    block turret

--- src/js/Block_Turrets.js
+++ src/js/Block_Turrets.js
@@ -1,31 +1,4 @@
 /*
-    Class File: Block.js
-    Inherits from: GameObject
-    Description: A block object that the player can collide with
-        Running into a block kills the player.
-		
-    Method Signatures:
-        Block(x,y)
-*/
-
-function Block(x,y) {
-  
-    this.x = x;
-    this.y = y;
-  
-    this.width  = 2 * TILE_SIZE;
-    this.height = 2 * TILE_SIZE;
-
-    this.sprite.src = "images/block/block3.png";                      
-
-    blocks.push(this); // Add self to manager array
-    
-}
-
-Block.prototype = new GameObject();
-Block.prototype.constructor = Block;
-
-/*
     Class File: Block_Turret.js
     Inherits from: GameObject
     Description: A block object that the player can collide with
